9cac04eb418dcacdc1f002a974f430efd998a0f0
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-05-09 02:33:59 +0200

    ci: configuration celery for tests

--- accounts/tests/conftest.py
+++ accounts/tests/conftest.py
@@ -3,6 +3,12 @@ import pytest
 from accounts.models import CustomUser, VerificationCode
 
 
+@pytest.fixture(autouse=True)
+def celery_eager(settings):
+    settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.CELERY_TASK_EAGER_PROPAGATES = True
+
+
 @pytest.fixture
 def test_user(db):
     return CustomUser.objects.create_user(email="test@test.com", password="Test_password")
