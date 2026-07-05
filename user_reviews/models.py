7fa1cc5556d1f38fef1bce3734bc0829b9a2d8d6
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-07-06 01:07:55 +0200

    Fix: migrations in user_reviews

--- user_reviews/models.py
+++ user_reviews/models.py
@@ -11,7 +11,7 @@ class Review(models.Model):
     comment = models.TextField(max_length=300, null=True, blank=True)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="reviews")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
