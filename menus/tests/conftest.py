d124b9e86872a5a5e47679712e7bf0d574ad2d93
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-07-09 00:48:28 +0200

    feat: Add all tests to DishViewSet

--- menus/tests/conftest.py
+++ menus/tests/conftest.py
@@ -1,13 +1,18 @@
 import pytest
 
-from menus.models import Menu
+from menus.models import Dish, Menu
 
 
 @pytest.fixture
 def test_menu(test_restaurant):
-    return Menu.objects.create(id=1, name="test_menu", restaurant=test_restaurant)
+    return Menu.objects.create(name="test_menu", restaurant=test_restaurant)
 
 
 @pytest.fixture
 def test_menu_1(test_restaurant_1):
-    return Menu.objects.create(id=2, name="test_menu_1", restaurant=test_restaurant_1)
+    return Menu.objects.create(name="test_menu_1", restaurant=test_restaurant_1)
+
+
+@pytest.fixture
+def test_dish(test_menu):
+    return Dish.objects.create(name="test_dish", menu=test_menu, price=100)
--- menus/tests/test_endpoints.py
+++ menus/tests/test_endpoints.py
@@ -75,11 +75,9 @@ def test_DishViewSet_post_requires_authentication():
     # menu id 1 = test_menu.id in file conftest.py
     [
         # Missing name
-        ({"name": "", "menu": 1, "price": 129.10}, status.HTTP_400_BAD_REQUEST),
-        # Missing menu
-        ({"name": "test_dish", "menu": "", "price": 129.10}, status.HTTP_400_BAD_REQUEST),
+        ({"name": "", "price": 129.10}, status.HTTP_400_BAD_REQUEST),
         # Missing price
-        ({"name": "test_dish", "menu": 1, "price": ""}, status.HTTP_400_BAD_REQUEST),
+        ({"name": "test_dish", "price": ""}, status.HTTP_400_BAD_REQUEST),
         # Not exist menu
         ({"name": "test_dish", "menu": 99999, "price": ""}, status.HTTP_400_BAD_REQUEST),
     ],
@@ -87,5 +85,106 @@ def test_DishViewSet_post_requires_authentication():
 def test_DishViewSet_post_invalid_payload(test_owner, test_menu, payload, excepted_status):
     client = APIClient()
     client.force_authenticate(user=test_owner)
+    payload["menu"] = payload.get("menu", test_menu.id)
     response = client.post("/api/menus/dish/", payload)
     assert response.status_code == excepted_status
+
+
+# Patch method
+def test_DishViewSet_patch_owner_of_restaurant(test_owner, test_dish):
+    """
+    test_owner is the owner of the restaurant that contains test_dish.
+    So he has access to this endpoint.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_owner)
+    body = {"name": "new_name"}
+    response = client.patch(f"/api/menus/dish/{test_dish.id}/", body)
+    test_dish.refresh_from_db()
+    assert response.status_code == 200
+    assert test_dish.name == body["name"]
+
+
+def test_DishViewSet_patch_manager_of_restaurant(test_membership_manager, test_dish):
+    """
+    In this test request user is the manager of the restaurant that contains test_dish.
+    So he has access to this endpoint.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_membership_manager.user)
+    body = {"price": test_dish.price + 50}
+    response = client.patch(f"/api/menus/dish/{test_dish.id}/", body)
+    test_dish.refresh_from_db()
+    assert response.status_code == 200
+    assert test_dish.price == body["price"]
+
+
+def test_DishViewSet_patch_staff_of_restaurant(test_membership_staff, test_dish):
+    """
+    In this test request user is only the staff of the restaurant so
+    he does not have access to this endpoint.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_membership_staff.user)
+    response = client.patch(f"/api/menus/dish/{test_dish.id}/", {})
+    assert response.status_code == 403
+
+
+def test_DishViewSet_patch_normal_user(test_user, test_dish):
+    client = APIClient()
+    client.force_authenticate(user=test_user)
+    response = client.patch(f"/api/menus/dish/{test_dish.id}/", {})
+    assert response.status_code == 403
+
+
+def test_DishViewSet_patch_requires_authentication():
+    client = APIClient()
+    response = client.patch("/api/menus/dish/", {})
+    assert response.status_code == 401
+
+
+# Delete method
+def test_DishViewSet_delete_owner_of_restaurant(test_owner, test_dish):
+    """
+    In this test request user is the owner of the restaurant that contains test_dish.
+    So he is allowed to delete a dish.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_owner)
+    response = client.delete(f"/api/menus/dish/{test_dish.id}/")
+    assert response.status_code == 204
+    assert not Dish.objects.filter(id=test_dish.id).exists()
+
+
+def test_DishViewSet_delete_manager_of_restaurant(test_membership_manager, test_dish):
+    """
+    In this test request user is the manager of the restaurant that contains test_dish.
+    So he is allowed to delete a dish.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_membership_manager.user)
+    response = client.delete(f"/api/menus/dish/{test_dish.id}/")
+    assert response.status_code == 204
+    assert not Dish.objects.filter(id=test_dish.id).exists()
+
+
+def test_DishViewSet_delete_staff_of_restaurant(test_membership_staff, test_dish):
+    """
+    In this test request user is only the staff of the restaurant so
+    he does not have access to this endpoint.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_membership_staff.user)
+    response = client.delete(f"/api/menus/dish/{test_dish.id}/")
+    assert response.status_code == 403
+
+
+def test_DishViewSet_delete_normal_user(test_user, test_dish):
+    """
+    In this test request user is just a normal user.
+    So he does not have access to this endpoint.
+    """
+    client = APIClient()
+    client.force_authenticate(user=test_user)
+    response = client.delete(f"/api/menus/dish/{test_dish.id}/")
+    assert response.status_code == 403
