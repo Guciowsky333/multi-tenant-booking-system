ec28fd2c04e638de9348f226721ad3f5a9f16aa9
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-07-28 12:19:16 +0200

    feat: Swagger Api documentation in memberships django application

--- memberships/views.py
+++ memberships/views.py
@@ -35,8 +35,8 @@ class MemberShipViewSet(viewsets.ModelViewSet):
         responses={
             200: OpenApiResponse(description="List of all members of the restaurant"),
             400: OpenApiResponse(description="User did not provide restaurant"),
-            401: OpenApiResponse(description="Unauthorized"),
-            403: OpenApiResponse(description="Forbidden"),
+            403: OpenApiResponse(description="User does not have permission to access this endpoint"),
+            401: OpenApiResponse(description="User is not authorized"),
         },
     )
     def list(self, request, *args, **kwargs):
@@ -47,3 +47,71 @@ class MemberShipViewSet(viewsets.ModelViewSet):
         queryset = MemberShip.objects.filter(restaurant=restaurant_id)
         serializer = MemberShipSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    @extend_schema(
+        summary="Create new membership",
+        description="""
+        Create membership with provided email at provided restaurant.
+        
+        Business rules:
+        - Field (restaurant, email, role) are required.
+        - Restaurant must exist.
+        - In body you should provide email instead of user_id. The 
+        serializer will automatically find the user with this email.
+        - User with provided email must exist and cannot be owner of provided restaurant
+        - Combination of restaurant and user must be unique (One user can have max 1 membership)
+        - Allowed role values : "manager", "staff".
+        - Request user must be authenticated.
+        - Request user must be owner of the restaurant.
+        """,
+        request=MemberShipSerializer,
+        responses={
+            201: OpenApiResponse(description="Membership created"),
+            400: OpenApiResponse(description="Validation error"),
+            403: OpenApiResponse(description="User does not have permission to access this endpoint"),
+            401: OpenApiResponse(description="User is not authorized"),
+        },
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Update membership role",
+        description="""
+        Updates role of existing membership.
+        Only 'role' field can be changed - restaurant and user cannot be modified after creation.
+
+        Business rules:
+        - Field role is required.
+        - Allowed role values: "manager", "staff".
+        - Request user must be authenticated.
+        - Request user must be owner of the restaurant.
+        """,
+        request=MembershipUpdateSerializer,
+        responses={
+            200: OpenApiResponse(description="Membership updated"),
+            400: OpenApiResponse(description="Validation error"),
+            403: OpenApiResponse(description="User does not have permission to access this endpoint"),
+            401: OpenApiResponse(description="User is not authorized"),
+            404: OpenApiResponse(description="Membership not found"),
+        },
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Partially updates membership role",
+        description="""
+        The same business rules as in update.
+        """,
+        request=MembershipUpdateSerializer,
+        responses={
+            200: OpenApiResponse(description="Membership updated"),
+            400: OpenApiResponse(description="Validation error"),
+            403: OpenApiResponse(description="User does not have permission to access this endpoint"),
+            401: OpenApiResponse(description="User is not authorized"),
+            404: OpenApiResponse(description="Membership not found"),
+        },
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
